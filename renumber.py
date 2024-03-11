4557a68dbd0628cf41c5c02ea86274e390e8e4a6
Drew Middlesworth <drew.middlesworth@gmail.com>
2024-03-11 11:13:42 -0700

    Add auto update for totals

--- renumber.py
+++ renumber.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 import os
+import re
 
-
+regex = re.compile('[0-9][0-9][0-9]*')
+total = 0
 for file in os.listdir():
     newfile = ''
     number = 1
@@ -18,7 +20,15 @@ for file in os.listdir():
                     else:
                         newline = '### #' + str(number) + ' ' + ' '.join(splitline[1:])
                     number += 1
+                    total += 1
                 else:
                     newline = line         
                 f.write(newline)
 
+with open('index.md') as f:
+    lines = f.readlines()
+with open('index.md', 'w') as f:
+    for line in lines:
+        if line.startswith('I'):
+            line = re.sub(regex, str(total), line)
+        f.write(line)
