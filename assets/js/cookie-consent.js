db6d12dea22015760d795cf08dab1bc35c218982
Drew Middlesworth <drew.middlesworth@gmail.com>
2025-12-12 23:29:06 -0800

    Try this way

--- assets/js/cookie-consent.js
+++ assets/js/cookie-consent.js
@@ -2,12 +2,25 @@ document.addEventListener("DOMContentLoaded", function () {
   const banner = document.getElementById("cookie-consent");
   const button = document.getElementById("accept-cookies");
 
-  if (!localStorage.getItem("cookieConsent")) {
+  function getCookie(name) {
+    return document.cookie
+      .split("; ")
+      .find(row => row.startsWith(name + "="))
+      ?.split("=")[1];
+  }
+
+  function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/; SameSite=Lax`;
+  }
+
+  if (!getCookie("cookie_consent")) {
     banner.style.display = "flex";
   }
 
   button.addEventListener("click", function () {
-    localStorage.setItem("cookieConsent", "true");
+    setCookie("cookie_consent", "true", 365);
     banner.style.display = "none";
   });
 });
